c061108e386c84933cb39c74fe7079f192bfdd50
上帝的梦 <1597691532@qq.com>
2025-08-29 12:04:15 +0800

    1

--- main.73a1d.js
+++ main.73a1d.js
@@ -111,14 +111,14 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = typeof loadRuntime === "function";
     if (isRuntime) {
-        require("src/settings.f8f39.js");
+        require("src/settings.a801a.js");
         require("src/cocos2d-runtime.js");
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require("src/physics.js");
         }
         require("jsb-adapter/engine/index.js");
     } else {
-        require("src/settings.f8f39.js");
+        require("src/settings.a801a.js");
         require("src/cocos2d-jsb.js");
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require("src/physics.js");
